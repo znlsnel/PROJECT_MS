a4162d54108d5d4bb3ebcd4a55a6bb56896491a1
znlsnel <dnel0605@gmail.com>
2025-04-24 19:28:22 +0900

    [Add] Character Asset

--- Assets/0. External/Character/Creative_Characters_FREE/Scripts/Editor/Randomizer/Steps/IRandomizerStep.cs
+++
@@ -1,9 +0,0 @@
-namespace CharacterCustomizationTool.Editor.Randomizer.Steps
-{
-    public interface IRandomizerStep
-    {
-        GroupType GroupType { get; }
-
-        StepResult Process(int count, GroupType[] groups);
-    }
-}
--- Assets/0. External/Character/Creative_Characters_FREE/Scripts/Editor/SlotValidation/ISlotValidationRules.cs
+++
@@ -1,9 +0,0 @@
-using CharacterCustomizationTool.Editor.Character;
-
-namespace CharacterCustomizationTool.Editor.SlotValidation
-{
-    public interface ISlotValidationRules
-    {
-        void Validate(CustomizableCharacter character, SlotType type, bool isToggled);
-    }
-}
