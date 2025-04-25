f1f7fdc5a10e672bfd59dbf7480be16f28432940
znlsnel <dnel0605@gmail.com>
2025-04-25 12:10:27 +0900

    [Feat] Quest System & UI
    - 퀘스트 시스템 및 UI

--- Assets/Scripts/5. UI/Quest/QuestTaskUI.cs
+++ Assets/Scripts/5. UI/Quest/QuestTaskUI.cs
@@ -7,8 +7,15 @@ public class QuestTaskUI : MonoBehaviour
     [SerializeField] private TextMeshProUGUI _title;
     [SerializeField] private TextMeshProUGUI _currentCount;
     [SerializeField] private TextMeshProUGUI _targetCount;
-    
+
+    private CanvasGroup _canvasGroup;
     private QuestTask _questTask;
+
+    private void Awake()
+    {
+        _canvasGroup = GetComponent<CanvasGroup>();
+    }
+
     public void Setup(QuestTask questTask)
     {
         this._questTask = questTask;
@@ -16,22 +23,23 @@ public class QuestTaskUI : MonoBehaviour
         _currentCount.text = questTask.progress.ToString();
         _targetCount.text = questTask.taskData.SuccessCount.ToString();
 
-        questTask.onSuccessChanged += UpdateProgress;
+        questTask.onProgressChanged += UpdateProgress;
     }
 
-    public void UpdateProgress(QuestTask questTask, bool isSuccess)
+    public void UpdateProgress(QuestTask questTask, int currentCount, int targetCount)
     {
-        _currentCount.text = questTask.progress.ToString();
-        _targetCount.text = questTask.taskData.SuccessCount.ToString();
+        _currentCount.text = currentCount.ToString();
+        _targetCount.text = targetCount.ToString();
 
-        if (isSuccess)
+        if (currentCount >= targetCount)
         {
             // TODO 색 정리
+            _canvasGroup.alpha = 0.5f;
         }
     }
 
     private void OnDestroy()
-    {
-        _questTask.onSuccessChanged -= UpdateProgress;
+    { 
+        _questTask.onProgressChanged -= UpdateProgress;
     }
 }
