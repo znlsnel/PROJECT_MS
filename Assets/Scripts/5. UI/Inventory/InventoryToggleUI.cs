efabf434091189d20a0e26a9498ad5b15844d54d
znlsnel <dnel0605@gmail.com>
2025-04-27 18:46:33 +0900

    [Wip]

--- Assets/Scripts/5. UI/Inventory/InventoryToggleUI.cs
+++ Assets/Scripts/5. UI/Inventory/InventoryToggleUI.cs
@@ -4,7 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using UnityEngine;
 
-public class InventoryTogleUI : MonoBehaviour
+public class InventoryToggleUI : MonoBehaviour
 {
-  
+    [SerializeField] private RectTransform CheckImage;
+    
 }
