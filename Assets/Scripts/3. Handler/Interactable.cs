34e7c6bfdd842ff9fdf3e0ea172111b510bacdee
Sudo0928 <57037151+Sudo0928@users.noreply.github.com>
2025-04-29 10:20:36 +0900

    [fix] 상호작용 오류 수정 및 로직 변경

--- Assets/Scripts/3. Handler/Interactable.cs
+++ Assets/Scripts/3. Handler/Interactable.cs
@@ -5,35 +5,5 @@ public abstract class Interactable : MonoBehaviour
     [field: SerializeField] public AnimationClip interactAnimation { get; private set; }
     [field: SerializeField] public float interactAnimationSpeed { get; private set; } = 1f;
 
-    public virtual void OnTriggerEnter(Collider other)
-    {
-        if(other.TryGetComponent(out InteractionHandler interactionHandler))
-        {
-            interactionHandler.AddInteractable(this);
-            Debug.Log("Trigger Enter");
-        }
-    }
-
-    public virtual void OnTriggerExit(Collider other)
-    {
-        if(other.TryGetComponent(out InteractionHandler interactionHandler))
-        {
-            interactionHandler.RemoveInteractable(this);
-            Debug.Log("Trigger Exit");
-        }
-    }
-
-    public virtual void OnDestroy()
-    {
-        Collider[] colliders = Physics.OverlapSphere(transform.position, 1f, LayerMask.GetMask("Player"));
-        foreach(Collider collider in colliders)
-        {
-            if(collider.TryGetComponent(out InteractionHandler interactionHandler))
-            {
-                interactionHandler.RemoveInteractable(this);
-            }
-        }
-    }
-
     public abstract void Interact(GameObject obj);
 }
