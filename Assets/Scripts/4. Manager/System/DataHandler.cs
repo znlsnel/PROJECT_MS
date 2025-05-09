ea441cd59faa81e3484ce508961e1b4fc12ff483
znlsnel <dnel0605@gmail.com>
2025-05-09 20:16:50 +0900

    [Fix] 파일 이름 수정
    - Data handler class 이름만 바꾸고 파일 이름 안바꾼 부분 수정

--- Assets/Scripts/4. Manager/System/DataHandler.cs
+++ Assets/Scripts/4. Manager/System/DataHandler.cs
@@ -1,120 +1,55 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using GoogleSheet;
-using GoogleSheet.Protocol.v2.Res;
-using System.Reflection;
-using UGS;
-using UnityEngine.Assertions;
-using System.Diagnostics;
-using UnityEngine;
-using Debug = UnityEngine.Debug;
+using System.Threading.Tasks;
 
-public class UGSDataHandler<T> where T : ITable
+
+public abstract class DataHandler<T> where T : class
 {
-    private readonly Dictionary<int, T> dataDictionary;
-    private readonly List<T> dataCollection;
-    private readonly Action<bool> loadAction;
-    private readonly Action<T, Action<WriteObjectResult>> writeAction;
+    protected Dictionary<int, T> datas = new Dictionary<int, T>();
+    protected List<T> dataList = new List<T>();
 
-    public UGSDataHandler()
+    public DataHandler()
     {
-        // UGS Data 클래스의 인스턴스 생성
-         //var dataInstance = Activator.CreateInstance<T>(); 
-        
-        // 리플렉션으로 필요한 멤버 가져오기 
-        var type = typeof(T);  
-        
-        // Dictionary와 List 필드 찾기
-        var dictionaryField = type.GetField($"{type.Name}Map", BindingFlags.Public | BindingFlags.Static); 
-        var listField = type.GetField($"{type.Name}List", BindingFlags.Public | BindingFlags.Static);
-        
-        Debug.Assert(dictionaryField != null, $"Required fields not found in type {type.Name}");
-        Debug.Assert(listField != null, $"Required fields not found in type {type.Name}"); 
-
-        // Load와 Write 메서드 찾기
-        var loadMethod = type.GetMethod("Load", BindingFlags.Public | BindingFlags.Static);
-        var writeMethod = type.GetMethod("Write", BindingFlags.Public | BindingFlags.Static);
-        
-        Debug.Assert(loadMethod != null, $"Required methods not found in type {type.Name}");
-        Debug.Assert(writeMethod != null, $"Required methods not found in type {type.Name}"); 
-
-        // 초기 데이터 로드
-        loadMethod.Invoke(null, new object[] { false });
-
-        // 데이터 초기화
-        dataDictionary = (Dictionary<int, T>)dictionaryField.GetValue(null);
-        dataCollection = (List<T>)listField.GetValue(null);
-        
-        loadAction = (forceReload) => loadMethod.Invoke(null, new object[] { forceReload });
-        writeAction = (data, callback) => writeMethod.Invoke(null, new object[] { data, callback });
+        Managers.SubscribeToInit(Init);
     }
 
-    /// <summary>
-    /// 인덱스로 데이터를 조회합니다.
-    /// </summary>
-    public T GetByIndex(int index)
-    {
-        if (dataDictionary.TryGetValue(index, out var item))
-        {
-            return item;
-        }
+    protected abstract void Init();
 
-        Debug.LogError($"{typeof(T).Name} Data not found: {index}");
-        return default;
-    }
-
-    /// <summary>
-    /// 모든 데이터를 반환합니다.
-    /// </summary>
+    
     public List<T> GetAll()
     {
-        return dataCollection;
+        return dataList;
     }
 
-    /// <summary>
-    /// 데이터 총 개수를 반환합니다.
-    /// </summary>
-    public int GetCount()
+    public T GetByIndex(int index)
     {
-        return dataCollection.Count;
+        if (datas.TryGetValue(index, out T data))
+            return data;
+
+        return null;
     }
 
-    /// <summary>
-    /// 조건에 맞는 데이터들을 필터링하여 반환합니다.
-    /// </summary>
-    public List<T> GetByCondition(Func<T, bool> predicate)
+    public T GetByCondition(Func<T, bool> condition)
     {
-        return dataCollection.Where(predicate).ToList();
-    }
-
-    /// <summary>
-    /// 데이터를 업데이트합니다.
-    /// </summary>
-    public void UpdateData(T data, Action<bool> onComplete = null)
-    {
-        if (data == null)
+        foreach (T data in dataList)
         {
-            onComplete?.Invoke(false);
-            return;
+            if (condition(data))
+                return data;
         }
 
-        writeAction(data, (result) =>
-        {
-            bool success = result != null && result.error == null;
-            if (success)
-            {
-                loadAction(true);
-            }
-            onComplete?.Invoke(success);
-        });
+        return null;
     }
 
-    /// <summary>
-    /// 데이터를 강제로 리로드합니다.
-    /// </summary>
-    public void Reload()
+    public List<T> GetAllByCondition(Func<T, bool> condition)
     {
-        loadAction(true);
+        List<T> items = new List<T>();
+        foreach (T data in dataList)
+        {
+            if (condition(data))
+                items.Add(data);
+        }
+
+        return items;
     }
 }
