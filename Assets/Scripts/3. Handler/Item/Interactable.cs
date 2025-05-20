a483828383e9f6fef79103d2c2802abf43311b47
Sudo0928 <57037151+Sudo0928@users.noreply.github.com>
2025-05-20 21:07:15 +0900

    [wip]

--- Assets/Scripts/3. Handler/Item/Interactable.cs
+++ Assets/Scripts/3. Handler/Item/Interactable.cs
@@ -1,6 +1,7 @@
+using FishNet.Object;
 using UnityEngine;
 
-public abstract class Interactable : MonoBehaviour
+public abstract class Interactable : NetworkBehaviour
 {
     [field: SerializeField] public AnimationClip interactAnimation { get; private set; }
     [field: SerializeField] public float interactAnimationSpeed { get; private set; } = 1f;
