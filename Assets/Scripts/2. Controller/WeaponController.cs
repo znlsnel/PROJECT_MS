adf6281c9a8ed2eedf217b6f1ea122178f77e6c4
znlsnel <dnel0605@gmail.com>
2025-05-28 21:01:49 +0900

    [Feat] FX

--- Assets/Scripts/2. Controller/WeaponController.cs
+++ Assets/Scripts/2. Controller/WeaponController.cs
@@ -11,6 +11,25 @@ public class WeaponController : ItemController
     [SerializeField] private float damage = 10f;
     private bool isAttacking;
 
+    private readonly static string fxHumanPath = "FX/PunchFX_Human.prefab";
+    private readonly static string fxPath = "FX/PunchFX.prefab";  
+
+
+    private static GameObject _fxHuman;
+    private static GameObject _fx;
+
+    private void Start()
+    {
+        if (_fxHuman == null)
+        {
+            _fxHuman = Resources.Load<GameObject>(fxHumanPath);
+        }
+        if (_fx == null)
+        {
+            _fx = Resources.Load<GameObject>(fxPath);
+        }
+    }
+
     public override void Setup(AlivePlayer owner, ItemSlot itemSlot)
     {
         base.Setup(owner, itemSlot);
@@ -29,6 +48,15 @@ public class WeaponController : ItemController
 
         if(other.gameObject.TryGetComponent(out IDamageable damageable))
         {
+            GameObject fx = null;
+            if (other.gameObject.GetComponent<AlivePlayer>() != null)
+                fx = Managers.Pool.Get(fxHumanPath);
+            else
+                fx = Managers.Pool.Get(fxPath);
+
+            fx.transform.position = transform.position; 
+            Managers.Pool.Release(fx, 2f);  
+
             if(!damageable.CanTakeDamage()) return;
             
             if(damageables.Add(damageable))
